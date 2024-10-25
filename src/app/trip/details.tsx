b107043e45645a1c43d4b13595387238dfd2c6c1
lucasptcastro <lucasptcastro@gmail.com>
2024-10-25 12:42:39 -0300

    ✨ feat: added button and modal

--- src/app/trip/details.tsx
+++ src/app/trip/details.tsx
@@ -1,9 +1,27 @@
+import { Button } from "@/components/Button"
+import { Modal } from "@/components/Modal/modal"
+import { colors } from "@/styles/colors"
+import { Plus } from "lucide-react-native"
+import { useState } from "react"
 import { Text, View } from "react-native"
 
 export function Details({ tripId }: { tripId: string }) {
+  // MODAL
+  const [showModal, setShowModal] = useState(false)
+
   return (
-    <View className="flex-1">
-      <Text className="text-white">{tripId}</Text>
+    <View className="mt-10 flex-1">
+      <Text className="mb-2 font-semibold text-2xl text-zinc-50">Links importantes</Text>
+
+      <View className="flex-1">
+        <Button variant="secondary">
+          <Plus color={colors.zinc[200]} size={20} />
+
+          <Button.Title>Cadastrar novo link</Button.Title>
+        </Button>
+      </View>
+
+      <Modal title="Cadastrar link" subtitle="Todos os convidados podem visualizar os links importantes." visible={showModal} onClose={() => setShowModal(false)}></Modal>
     </View>
   )
 }
