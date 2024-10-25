56da079c884966174d21779d74d1c984244e71b0
lucasptcastro <lucasptcastro@gmail.com>
2024-10-25 15:02:04 -0300

    ✨ feat: added function to create link

--- src/app/trip/details.tsx
+++ src/app/trip/details.tsx
@@ -1,27 +1,85 @@
-import { Button } from "@/components/Button"
-import { Modal } from "@/components/Modal/modal"
-import { colors } from "@/styles/colors"
-import { Plus } from "lucide-react-native"
 import { useState } from "react"
-import { Text, View } from "react-native"
+import { Plus } from "lucide-react-native"
+import { Alert, Text, View } from "react-native"
+
+import { Button } from "@/components/Button"
+import { Input } from "@/components/Input"
+import { Modal } from "@/components/Modal/modal"
+
+import { linksServer } from "@/server/links-server"
+
+import { validateInput } from "@/utils/validateInput"
+import { colors } from "@/styles/colors"
 
 export function Details({ tripId }: { tripId: string }) {
   // MODAL
-  const [showModal, setShowModal] = useState(false)
+  const [showNewLinkModal, setShowNewLinkModal] = useState(false)
+
+  // LOADING
+  const [isCreatingLinkTrip, setIsCreatingLinkTrip] = useState(false)
+
+  // DATA
+  const [linkTitle, setLinkTitle] = useState("")
+  const [linkURL, setLinkURL] = useState("")
+
+  function resetNewLinkFields() {
+    setLinkTitle("")
+    setLinkURL("")
+    setShowNewLinkModal(false)
+  }
+
+  async function handleCreateTripLink() {
+    try {
+      if (!validateInput.url(linkURL.trim())) return Alert.alert("Link", "Link inválido")
+
+      if (!linkTitle.trim()) return Alert.alert("Link", "Informe o título do link.")
+
+      setIsCreatingLinkTrip(true)
+
+      await linksServer.create({ tripId, title: linkTitle, url: linkURL })
+
+      Alert.alert("Link", "Link criado com sucesso!")
+
+      resetNewLinkFields()
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsCreatingLinkTrip(false)
+    }
+  }
 
   return (
     <View className="mt-10 flex-1">
       <Text className="mb-2 font-semibold text-2xl text-zinc-50">Links importantes</Text>
 
       <View className="flex-1">
-        <Button variant="secondary">
+        <Button variant="secondary" onPress={() => setShowNewLinkModal(true)}>
           <Plus color={colors.zinc[200]} size={20} />
 
           <Button.Title>Cadastrar novo link</Button.Title>
         </Button>
       </View>
 
-      <Modal title="Cadastrar link" subtitle="Todos os convidados podem visualizar os links importantes." visible={showModal} onClose={() => setShowModal(false)}></Modal>
+      <Modal
+        title="Cadastrar link"
+        subtitle="Todos os convidados podem visualizar os links importantes."
+        visible={showNewLinkModal}
+        onClose={() => setShowNewLinkModal(false)}
+      >
+        <View className="mb-3 gap-2">
+          <Input variant="secondary">
+            <Input.Field placeholder="Título do link" onChangeText={setLinkTitle} />
+          </Input>
+
+          <Input variant="secondary">
+            <Input.Field placeholder="URL" onChangeText={setLinkURL} />
+          </Input>
+        </View>
+
+        <Button isLoading={isCreatingLinkTrip} onPress={handleCreateTripLink}>
+          <Button.Title>Salvar link</Button.Title>
+        </Button>
+      </Modal>
     </View>
   )
 }
