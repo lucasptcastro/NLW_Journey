a058e1c949f27d54900dfb5b83f392aa18d5ffe6
lucasptcastro <lucasptcastro@gmail.com>
2024-10-25 10:39:16 -0300

    ✨ feat: added comment

--- src/components/Activity/activity.tsx
+++ src/components/Activity/activity.tsx
@@ -18,22 +18,14 @@ type Props = {
 export function Activity({ data }: Props) {
   return (
     <View
-      className={clsx(
-        "w-full bg-zinc-900 px-4 py-3 rounded-lg flex-row items-center border border-zinc-800 gap-3",
-        { "opacity-50": data.isBefore }
-      )}
+      className={clsx("w-full flex-row items-center gap-3 rounded-lg border border-zinc-800 bg-zinc-900 px-4 py-3", { "opacity-50": data.isBefore })}
     >
-      {data.isBefore ? (
-        <CircleCheck color={colors.lime[300]} size={20} />
-      ) : (
-        <CircleDashed color={colors.zinc[400]} size={20} />
-      )}
+      {/* se a data da atividade for antes da data atual mostra um ícone concluído. Senão, mostra um ícone pendente */}
+      {data.isBefore ? <CircleCheck color={colors.lime[300]} size={20} /> : <CircleDashed color={colors.zinc[400]} size={20} />}
 
-      <Text className="text-zinc-100 font-regular text-base flex-1">
-        {data.title}
-      </Text>
+      <Text className="flex-1 font-regular text-base text-zinc-100">{data.title}</Text>
 
-      <Text className="text-zinc-400 font-regular text-sm">{data.hour}</Text>
+      <Text className="font-regular text-sm text-zinc-400">{data.hour}</Text>
     </View>
   )
 }
